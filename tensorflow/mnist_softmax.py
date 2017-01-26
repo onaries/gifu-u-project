b1626a90deb43617d069731637d1c8ee85b641c5
Seonwoo Kim Laptop <ksw8954@gmail.com>
2017-01-26 17:55:14 +0900

    YOLO

--- tensorflow/mnist_softmax.py
+++ tensorflow/mnist_softmax.py
@@ -33,6 +33,6 @@ for i in range(1000):
 # evalutating model
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        
+
 print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
